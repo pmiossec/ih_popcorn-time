54491be9791d98e8a598d1e01ec6d4b97d00ad82
Philippe Miossec <pmiossec@gmail.com>
2023-06-21 13:06:18 +0200

    Better message when no movies available

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import "./Header.css";
 
 export default function Header({movieCount}) {
+    const message = movieCount === 0
+        ? "New movie shooting now and will arrive soon...."
+        : `Number of movies: ${movieCount}`;
     return (
         <header className="Header">
             <h1>🍿 Popcorn time! 🍿</h1>
-            <h2>🎥 Number of movies: {movieCount}<span className="mirror">🎥</span></h2>
+            <h2>🎥 {message}<span className="mirror">🎥</span></h2>
         </header>
     )
 }
