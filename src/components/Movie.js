05eaed39a4321a841fa2e68cfb7b4da1d80dc0d7
Philippe Miossec <pmiossec@gmail.com>
2023-06-21 11:12:29 +0200

    Display rating

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,8 @@
-export default function Movie({id, title, imgURL, deleteMovie}) {
+export default function Movie({id, title, imgURL, rating, deleteMovie}) {
     return <div key={id} className="card">
                 <p>{title}</p>
                 <img src={imgURL} alt={title} />
+                <p>{'⭐'.repeat(rating)}<span className="black-star">{'⭐'.repeat(10-rating)}</span></p>
                 <button onClick={() => deleteMovie(id)}>Delete this movie</button>
             </div>
 }
