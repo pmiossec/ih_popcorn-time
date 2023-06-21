0e6d588469bb677d7284b3163fdc62a6809dcb4c
Philippe Miossec <pmiossec@gmail.com>
2023-06-21 12:46:09 +0200

    Better overhaul display

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import "./Header.css";
 export default function Header({movieCount}) {
     return (
         <header className="Header">
-            <h1>Popcorn time!</h1>
-            <h2>Number of movies: {movieCount}</h2>
+            <h1>🍿 Popcorn time! 🍿</h1>
+            <h2>🎥 Number of movies: {movieCount}<span className="mirror">🎥</span></h2>
         </header>
     )
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,8 @@
 export default function Movie({id, title, imgURL, rating, deleteMovie}) {
+    const imageUrl = imgURL ?? "https://placehold.co/300x440?text=No+poster+available";
     return <div className="card">
                 <p>{title}</p>
-                <img src={imgURL} alt={title} />
+                <img src={imageUrl} alt={title} />
                 <p>{'⭐'.repeat(rating)}<span className="black-star">{'⭐'.repeat(10-rating)}</span></p>
                 <button onClick={() => deleteMovie(id)}>Delete this movie</button>
             </div>
