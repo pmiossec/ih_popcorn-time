53f2917c88a7821cf88800a2ea8bc3d8de9b8e86
Philippe Miossec <pmiossec@gmail.com>
2023-06-21 11:07:07 +0200

    add movie images

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,7 @@
-export default function Movie({id, title, deleteMovie}) {
+export default function Movie({id, title, imgURL, deleteMovie}) {
     return <div key={id} className="card">
                 <p>{title}</p>
+                <img src={imgURL} alt={title} />
                 <button onClick={() => deleteMovie(id)}>Delete this movie</button>
             </div>
 }
