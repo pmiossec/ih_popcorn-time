21fc6954ae487494b5faf5c5dd61da8987fd977e
Philippe Miossec <pmiossec@gmail.com>
2023-06-21 10:13:24 +0200

    Display list of movies

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+import allMovies from '../data/movies.json'
+import './Main.css';
+
+
 export default function Main() {
+
+    const [movies, updateMovies ] = useState(allMovies);
+    const deleteMovie = id => {
+        updateMovies(movies.filter(m => m.id !== id));
+    }
+
     return (
-        <div>this is the MAIN</div>
+        <div className="Main">
+            { movies.map(m => <div key={m.id} className="card">
+            <p>{m.title}</p>
+                <button onClick={() => deleteMovie(m.id)}>Delete this movie</button>
+            </div>)}
+        </div>
     )
 }
