84b1e05b75005afde7d249f790d1f110e45cd39c
Philippe Miossec <pmiossec@gmail.com>
2023-06-21 15:06:06 +0200

    Add "Add a movie" feature

--- src/App.js
+++ src/App.js
@@ -9,13 +9,49 @@ import allMovies from './data/movies.json'
 
 function App() {
   const [movies, updateMovies ] = useState(allMovies);
+  const [title, setTitle ] = useState("");
+  const [rating, setRating ] = useState(0);
 
   const deleteMovie = id => {
     updateMovies(movies.filter(m => m.id !== id));
-}
+  }
+
+  const handleFormSubmit = (event) => {
+    console.log("event", event);
+    event.preventDefault();
+    const newMovie =    {
+      id: 42,
+      title: title,
+      year: 1972,
+      genres: ["Crime", "Drama"],
+      imgURL:"https://placehold.co/300x440?text=" + title,
+      rating: rating
+  };
+    updateMovies([newMovie, ...movies]);
+    setTitle("");
+  }
+
   return (
     <div className="App">
       <Header movieCount={movies.length} />
+
+      <form onSubmit={handleFormSubmit}>
+
+        <label>
+          Title:
+          <input type="text" name="title" placeholder='Enter movie title...'
+           value={title} onChange={e => setTitle(e.target.value)} />
+        </label>
+
+        <label>
+          Rating:
+          <input type="number" name="rating" placeholder='Enter movie rating...'
+           value={rating} onChange={e => setRating(e.target.value)} min={0} max={10} />
+        </label>
+        <button type="submit">Add a movie</button>
+      </form>
+
+
       <Main movies={movies} deleteMovie={deleteMovie} />
       <Footer />
     </div>
